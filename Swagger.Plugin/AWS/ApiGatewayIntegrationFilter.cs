a28737ec2c58f91f2185659382a807fc8ac4702f
Vikas Chaturvedi <38682308+VikasChaturvedi87@users.noreply.github.com>
2018-04-26 12:50:07 +0530

    Swagger plugin for AWS Web Api integration.

---
+++ Swagger.Plugin/AWS/ApiGatewayIntegrationFilter.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace Swagger.Plugin.AWS {
+    public class ApiGatewayIntegrationFilter : IOperationFilter {
+
+        /// <summary>
+        /// Gets or sets the integration settings.
+        /// </summary>
+        /// <value>
+        /// The integration settings.
+        /// </value>
+        public  static ApiGatewayIntegrationSettings IntegrationSettings { get; set; }
+
+        /// <summary>
+        /// Applies the specified operation.
+        /// </summary>
+        /// <param name="operation">The operation.</param>
+        /// <param name="schemaRegistry">The schema registry.</param>
+        /// <param name="apiDescription">The API description.</param>
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription) {
+            if (apiDescription.HttpMethod == System.Net.Http.HttpMethod.Options) {
+                CreateOptionsMethod(operation);
+                return;
+            }
+
+            //Add extra response for every opration
+            foreach (var extraResponse in IntegrationSettings.extraResponses) {
+                if (!operation.responses.ContainsKey(extraResponse.Key)) {
+                    operation.responses.Add(extraResponse);
+                }
+            }
+
+            if (operation.parameters == null) {
+                operation.parameters = new List<Parameter>();
+            }
+
+            foreach (var parameter in IntegrationSettings.globalActionParameters) {
+                operation.parameters.Add(parameter);
+            }
+
+            foreach (var response in operation.responses) {
+
+                if (response.Value.headers == null) {
+                    response.Value.headers = new Dictionary<string, Header>();
+                }
+
+                if (response.Value.vendorExtensions == null) {
+                    response.Value.vendorExtensions = new Dictionary<string, object>();
+                }
+
+                response.Value.description = response.Key;
+
+                foreach (var allowedAccessControl in IntegrationSettings.allowedAccessControl.GetAllowedAccessControls()) {
+                    response.Value.headers.Add(allowedAccessControl, new Header() { type = "string" });
+                }
+
+            }
+
+            operation.vendorExtensions.Add("x-amazon-apigateway-integration", new
+            {
+                type = "http",
+                httpMethod = apiDescription.HttpMethod.Method.ToUpper(),
+                uri = $"{HttpContext.Current.Request.Url.Scheme}://{HttpContext.Current.Request.Url.Host}{System.Web.Hosting.HostingEnvironment.ApplicationVirtualPath.TrimEnd('/')}/{apiDescription.RelativePathSansQueryString()}",
+                responses = GetAWSResponse(operation),
+                requestParameters = GetAWSRequest(operation)
+            });
+        }
+
+        /// <summary>
+        /// Gets the aws request.
+        /// </summary>
+        /// <param name="operation">The operation.</param>
+        /// <returns></returns>
+        private object GetAWSRequest(Operation operation) {
+            var requestParameters = new Dictionary<string, string>();
+            var methodParameterFormat = "method.request.{0}.{1}";
+            var integrationParameterFormat = "integration.request.{0}.{1}";
+
+            if (operation.parameters == null || operation.parameters.Count <= 0) return requestParameters;
+            foreach (var parameter in operation.parameters) {
+                switch (parameter.@in) {
+                    case "query":
+                        requestParameters.Add(string.Format(integrationParameterFormat, "querystring", parameter.name),
+                            string.Format(methodParameterFormat, "querystring", parameter.name));
+                        break;
+                    case "header":
+                        requestParameters.Add(string.Format(integrationParameterFormat, "header", parameter.name),
+                            string.Format(methodParameterFormat, "header", parameter.name));
+                        break;
+                }
+            }
+
+            return requestParameters;
+        }
+
+        private object GetAWSResponse(Operation operation) {
+            var awsResponses = new Dictionary<string, object>();
+            var responseParameters = new Dictionary<string, string>();
+
+            if (IntegrationSettings.allowedAccessControl.AllowHeaders)
+                responseParameters.Add("method.response.header.Access-Control-Allow-Headers", $"'{IntegrationSettings.allowedAccessControl.HeaderValue}'");
+
+            if (IntegrationSettings.allowedAccessControl.AllowMethods)
+                responseParameters.Add("method.response.header.Access-Control-Allow-Methods", $"'{IntegrationSettings.allowedAccessControl.MethodValue}'");
+
+            if (IntegrationSettings.allowedAccessControl.AllowOrigin)
+                responseParameters.Add("method.response.header.Access-Control-Allow-Origin", $"'{IntegrationSettings.allowedAccessControl.OriginValue}'");
+
+            foreach (var response in operation.responses) {
+                awsResponses.Add(response.Key, new
+                {
+                    statusCode = response.Key,
+                    responseParameters = responseParameters
+                });
+            }
+
+            awsResponses.Add("default", new
+            {
+                statusCode = "200",
+                responseParameters = responseParameters
+            });
+
+            return awsResponses;
+        }
+
+        private void CreateOptionsMethod(Operation operation) {
+            foreach (var response in operation.responses) {
+
+                if (response.Value.headers == null) {
+                    response.Value.headers = new Dictionary<string, Header>();
+                }
+
+                if (response.Value.vendorExtensions == null) {
+                    response.Value.vendorExtensions = new Dictionary<string, object>();
+                }
+
+                foreach (var allowedAccessControl in IntegrationSettings.allowedAccessControl.GetAllowedAccessControls()) {
+                    response.Value.headers.Add(allowedAccessControl, new Header() { type = "string" });
+                }
+            }
+
+            var responseParameters = new Dictionary<string, string>();
+            var awsResponses = new Dictionary<string, object>();
+            var requestTemplates = new Dictionary<string, string>();
+
+            operation.consumes.Add("application/json");
+            operation.consumes.Add("text/json");
+
+            operation.operationId = null;
+
+            requestTemplates.Add("application/json", "{\"statusCode\": 200}");
+
+            if (IntegrationSettings.allowedAccessControl.AllowHeaders)
+                responseParameters.Add("method.response.header.Access-Control-Allow-Headers", $"'{IntegrationSettings.allowedAccessControl.HeaderValue}'");
+
+            if (IntegrationSettings.allowedAccessControl.AllowMethods)
+                responseParameters.Add("method.response.header.Access-Control-Allow-Methods", $"'{IntegrationSettings.allowedAccessControl.MethodValue}'");
+
+            if (IntegrationSettings.allowedAccessControl.AllowOrigin)
+                responseParameters.Add("method.response.header.Access-Control-Allow-Origin", $"'{IntegrationSettings.allowedAccessControl.OriginValue}'");
+
+            awsResponses.Add("default", new
+            {
+                statusCode = "200",
+                responseParameters = responseParameters
+            });
+
+            operation.vendorExtensions.Add("x-amazon-apigateway-integration", new
+            {
+                type = "mock",
+                responses = awsResponses,
+                passthroughBehavior = "when_no_match",
+                requestTemplates = requestTemplates
+            });
+        }
+    }
+}
---
+++ Swagger.Plugin/AWS/ApiGatewayIntegrationSettings.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using Swashbuckle.Swagger;
+
+namespace Swagger.Plugin.AWS {
+    public class ApiGatewayIntegrationSettings {
+
+        /// <summary>
+        /// The extra responses
+        /// </summary>
+        internal IDictionary<string, Response> extraResponses;
+
+
+        /// <summary>
+        /// Gets or sets the global action parameters.
+        /// </summary>
+        /// <value>
+        /// The global action parameters.
+        /// </value>
+        /// <example>Authorization</example>
+        internal IList<Parameter> globalActionParameters { get; set; }
+
+        /// <summary>
+        /// The allowed access control
+        /// </summary>
+        internal AccessControl allowedAccessControl;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiGatewayIntegrationSettings"/> class.
+        /// </summary>
+        public ApiGatewayIntegrationSettings() {
+            extraResponses = new Dictionary<string, Response>();
+            allowedAccessControl = new AccessControl();
+            globalActionParameters = new List<Parameter>();
+        }
+
+        /// <summary>
+        /// Adds the response. Add only those responses which are not decorated on action methods.
+        /// </summary>
+        /// <param name="responseCode">The response code.</param>
+        /// <param name="response">The response.</param>
+        /// <exception cref="ArgumentException">response can not be empty!</exception>
+        public void AddResponse(string responseCode, Response response) {
+            if (string.IsNullOrWhiteSpace(responseCode))
+                throw new ArgumentException("response can not be empty!");
+
+            if (!extraResponses.ContainsKey(responseCode))
+                extraResponses.Add(responseCode, response ?? new Response());
+        }
+
+        /// <summary>
+        /// Adds the global action parameter.
+        /// </summary>
+        /// <param name="parameter">The parameter.</param>
+        /// <exception cref="ArgumentException">response can not be empty!</exception>
+        public void AddGlobalActionParameter(Parameter parameter) {
+            if (parameter != null)
+                globalActionParameters.Add(parameter);
+        }
+
+        public AccessControl AccessControlSettings()
+        {
+            return allowedAccessControl;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class AccessControl {
+            /// <summary>
+            /// The access control allow headers
+            /// </summary>
+            internal const string ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+            /// <summary>
+            /// The access control allow methods
+            /// </summary>
+            internal const string ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+            /// <summary>
+            /// The access control allow origin
+            /// </summary>
+            internal const string ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+
+            /// <summary>
+            /// Gets or sets a value indicating whether [allow headers].
+            /// </summary>
+            /// <value>
+            ///   <c>true</c> if [Access-Control-Allow-Headers]; otherwise, <c>false</c>.
+            /// </value>
+            internal bool AllowHeaders { get; set; }
+            /// <summary>
+            /// Gets or sets a value indicating whether [allow methods].
+            /// </summary>
+            /// <value>
+            ///   <c>true</c> if [Access-Control-Allow-Methods]; otherwise, <c>false</c>.
+            /// </value>
+            internal bool AllowMethods { get; set; }
+            /// <summary>
+            /// Gets or sets a value indicating whether [allow origin].
+            /// </summary>
+            /// <value>
+            ///   <c>true</c> if [Access-Control-Allow-Origin]; otherwise, <c>false</c>.
+            /// </value>
+            internal bool AllowOrigin { get; set; }
+
+            /// <summary>
+            /// Gets or sets the header value.
+            /// </summary>
+            /// <value>
+            /// The header value.
+            /// </value>
+            internal string HeaderValue { get; set; }
+            /// <summary>
+            /// Gets or sets the header methods.
+            /// </summary>
+            /// <value>
+            /// The header methods.
+            /// </value>
+            internal string MethodValue { get; set; }
+            /// <summary>
+            /// Gets or sets the header origin.
+            /// </summary>
+            /// <value>
+            /// The header origin.
+            /// </value>
+            internal string OriginValue { get; set; }
+
+            /// <summary>
+            /// Gets the allowed access controls.
+            /// </summary>
+            /// <returns></returns>
+            /// 
+            internal IList<string> GetAllowedAccessControls() {
+                var allowedAccessControl = new List<string>();
+
+                if (AllowOrigin)
+                    allowedAccessControl.Add(ACCESS_CONTROL_ALLOW_ORIGIN);
+                if (AllowMethods)
+                    allowedAccessControl.Add(ACCESS_CONTROL_ALLOW_METHODS);
+                if (AllowHeaders)
+                    allowedAccessControl.Add(ACCESS_CONTROL_ALLOW_HEADERS);
+
+                return allowedAccessControl;
+            }
+
+            /// <summary>
+            /// Headerses the wtih value.
+            /// </summary>
+            /// <param name="headerValue">The header value.</param>
+            /// <example>Content-Type,X-Amz-Date,Authorization,X-Api-Key</example>
+            /// <returns></returns>
+            public AccessControl AllowHeadersWtihValue(string headerValue)
+            {
+                AllowHeaders = true;
+                HeaderValue = headerValue;
+                return this;
+            }
+
+            /// <summary>
+            /// Allows the method wtih value.
+            /// </summary>
+            /// <param name="methodValue">The method value.</param>
+            /// <example>*</example>
+            /// <returns></returns>
+            public AccessControl AllowMethodWtihValue(string methodValue) {
+                AllowMethods = true;
+                MethodValue = methodValue;
+                return this;
+            }
+
+            /// <summary>
+            /// Allows the origen wtih value.
+            /// </summary>
+            /// <param name="origenValue">The origen value.</param>
+            /// <example>*</example>
+            /// <returns></returns>
+            public AccessControl AllowOrigenWtihValue(string origenValue) {
+                AllowOrigin = true;
+                OriginValue = origenValue;
+                return this;
+            }
+
+        }
+    }
+}
